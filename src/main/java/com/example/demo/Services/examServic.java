baab8992114ff261fcbcf380c177cb5e30081126
waelalq <wael.abuomr@gmail.com>
2022-12-03 00:43:06 +0200

    repo and serice of exam

--- src/main/java/com/example/demo/Services/examServic.java
+++ src/main/java/com/example/demo/Services/examServic.java
@@ -1,8 +1,30 @@
 package com.example.demo.Services;
 
+import com.example.demo.Entity.exams;
+import com.example.demo.Rebosotory.examRebosotory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class examServic {
+    @Autowired
+    examRebosotory examRebosotory;
+
+    public List<exams>allExams(){
+        return  examRebosotory.findAll();
+    }
+    public Optional<exams> showExam(Long id){
+        return examRebosotory.findById(id);
+
+    }
+
+    public  void  createExam(exams exam){
+        examRebosotory.save(exam);
+    }
+
+
 
 }
