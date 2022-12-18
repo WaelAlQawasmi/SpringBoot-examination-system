a03a35f6e31e1421b458e3fb05b369887ca8a2bb
waelalq <wael.abuomr@gmail.com>
2022-12-18 23:03:32 +0200

    finish  authantacation

--- src/main/java/com/example/demo/publicController.java
+++ src/main/java/com/example/demo/publicController.java
@@ -1,10 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.Entity.users;
+import com.example.demo.Repositories.userRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.security.Principal;
 
 @Controller
 public class publicController {
+    @Autowired
+    userRepository userRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
     @GetMapping("/")
     public String index() {
         return "index";
@@ -20,11 +34,29 @@ public class publicController {
     public String signup() {
         return "signup";
     }
+    @PostMapping("/signup")
+    public RedirectView signupPost(@RequestParam String password, @RequestParam String email, @RequestParam String name) {
+
+        if (userRepository.findByemail(email) == null) {
+            String passwordEncoded = passwordEncoder.encode(password);
+            users user = new users(name, email, passwordEncoded);
+            userRepository.save(user);
+            return new RedirectView("/login");
+        } else {
+            return new RedirectView("/signup?taken=true");
+        }
+    }
 
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard(Principal p, Model model) {
+String name= userRepository.findByemail(p.getName()).getName();
+
+        model.addAttribute("userInfo",name);
+
         return "dashboard";
     }
+
+
 
     @GetMapping("/exams")
     public String exams() {
